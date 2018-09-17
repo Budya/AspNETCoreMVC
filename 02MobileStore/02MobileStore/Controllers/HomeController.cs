74a8ff268e423576d159a51f71209f3cdb049b1f
Budya <budya2012@gmail.com>
2018-09-17 16:50:22 +0300

    ContentResult и JsonResult

---
+++ 02MobileStore/02MobileStore/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _02MobileStore.Models;
+using _02MobileStore.Util;
+
+namespace _02MobileStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MobileContext db;
+
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // save shanges
+            db.SaveChanges();
+            return "Thanks, " + order.User + ", for your order!";
+        }
+
+        public HtmlResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет ASP.NET Core<h2>");
+        }
+
+        public IActionResult Square(int altitude, int height)
+        {
+            //Для отправки ContentResult не надо использовать конструктор, 
+            //так как в контроллере уже определен специальный метод Content(),
+            //который принимает отправляемую строку и создает объект ContentResult.
+            double square = altitude * height / 2;
+            return Content($"Площадь треугольника с основанием {altitude} и высотой {height} равна {square}");
+        }
+
+        //для отправки клиенту объекта User мы можем написать следующий метод:
+        public JsonResult GetUser()
+        {
+            User user = new User { Name = "Tom", Age = 28 };
+            return Json(user);
+        }
+    }
+}
---
+++ 02MobileStore/02MobileStore/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using _02MobileStore.Models;
+
+namespace _02MobileStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<MobileContext>();
+                    SampleData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Во время создания базы данных произошла ошибка");
+                }
+            }
+            host.Run();
+            
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
