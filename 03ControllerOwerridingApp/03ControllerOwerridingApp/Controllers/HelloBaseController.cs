25b37425ed48b5f8e583515cbb4afac6bfeb18ef
Budya <budya2012@gmail.com>
2018-09-18 13:34:19 +0300

    04ControllerOwerriding

---
+++ 03ControllerOwerridingApp/03ControllerOwerridingApp/Controllers/HelloBaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace _03ControllerOwerridingApp.Controllers
+{
+    public abstract class HelloBaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Request.Headers.ContainsKey("User-Agent") &&
+                Regex.IsMatch(input: context.HttpContext.Request.Headers["User-Agent"].FirstOrDefault(), pattern: "MSIE 8.0"))
+            {
+                context.Result = Content("Internet Explorer 8.0 не поддерживается");
+            }
+            base.OnActionExecuting(context);
+        }
+        
+    }
+}
---
+++ SendFileApp/SendFileApp/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SendFileApp.Models;
+using Microsoft.AspNetCore.Hosting; // for IHostingEnvironment
+using System.IO;//for Path.Combine
+
+namespace SendFileApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _appEnvironment;
+
+        public HomeController(IHostingEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult GetFile()
+        {
+            // File path
+            string file_path = Path.Combine(_appEnvironment.ContentRootPath, "Files/book.pdf");
+            // File type - content-type
+            string file_type = "application/pdf";
+            // File name - can be unset
+            string file_name = "book.pdf";
+            return PhysicalFile(file_path, file_type, file_name);
+        }
+
+        // Sending array of bytes
+        public FileResult GetBytes()
+        {
+            string path = Path.Combine(_appEnvironment.ContentRootPath, "Files/book.pdf");
+            byte[] mas = System.IO.File.ReadAllBytes(path);
+            string file_type = "application/pdf";
+            string file_name = "book.pdf";
+            return File(mas, file_type, file_name);
+        }
+
+        // Sending Stream
+        public FileResult GetStream()
+        {
+            string path = Path.Combine(_appEnvironment.ContentRootPath, "Files/book.pdf");
+            FileStream fs = new FileStream(path, FileMode.Open);
+            string file_type = "application/pdf";
+            string file_name = "book3.pdf";
+            return File(fs, file_type, file_name);
+        }
+
+        public VirtualFileResult GetVirtualFile()
+        {
+            var filepath = Path.Combine("~/Files", "hello.txt");
+            return File(filepath, "text/plain", "hello.txt");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
