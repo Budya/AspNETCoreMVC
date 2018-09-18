d668a5f1fdbb6f0992ded77eb5a054bec5573274
Budya <budya2012@gmail.com>
2018-09-18 15:51:20 +0300

    Передача зависимостей в контроллер

---
+++ 05DependenciesInjInControllerApp/05DependenciesInjInControllerApp/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using _05DependenciesInjInControllerApp.Models;
+
+namespace _05DependenciesInjInControllerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITimeService _timeService;
+
+        public HomeController(ITimeService timeService)
+        {
+            _timeService = timeService;
+        }
+        // передача сервисов через конструктор
+        public string Index()
+        {
+            return _timeService.Time;
+        }
+
+        // передача зависимостей в методы. FromServices
+        public string Index1([FromServices] ITimeService timeService)
+        {
+            return timeService.Time;
+        }
+
+        // HttpContext.RequestServices
+        public string Index2()
+        {
+            ITimeService timeService = HttpContext.RequestServices.GetService<ITimeService>();
+            return timeService?.Time;
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
