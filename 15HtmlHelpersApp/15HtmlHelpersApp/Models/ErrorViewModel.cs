69aa3f87b017e3eff0a3d8fdcdde3cc7630e157e
Budya <budya2012@gmail.com>
2018-09-24 16:58:11 +0300

    HtmlHelpers

---
+++ 15HtmlHelpersApp/15HtmlHelpersApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _15HtmlHelpersApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
