c86be21a2bc7c51183231286534e8f1f2d40c230
Budya <budya2012@gmail.com>
2018-09-25 16:06:33 +0300

    TagHelperApp

---
+++ 16TagHelpersApp/16TagHelpersApp/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _16TagHelpersApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace _16TagHelpersApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        IEnumerable<Company> companies = new List<Company>
+        {
+            new Company { Id = 1, Name = "Apple" },
+            new Company { Id = 2, Name = "Samsung" },
+            new Company { Id=3, Name="Microsoft" }
+        };
+
+        public IActionResult Create()
+        {
+            ViewBag.Companies = new SelectList(companies, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public string Create(Phone phone)
+        {
+            Company company = companies.FirstOrDefault(c=> c.Id == phone.CompanyId);
+            return $"Добавлен новый элемент: {phone.Name} ({company?.Name})";
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(DayTimeViewModel model)
+        {
+            return Content(model.Period.ToString());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 16TagHelpersApp/16TagHelpersApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _16TagHelpersApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
