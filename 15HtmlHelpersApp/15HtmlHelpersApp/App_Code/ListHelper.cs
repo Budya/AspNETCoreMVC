0ee3f95272041a944a4f6135947ed65196e2e51c
Budya <budya2012@gmail.com>
2018-09-25 11:53:20 +0300

    HtmlHelpers App

--- 15HtmlHelpersApp/15HtmlHelpersApp/App_Code/ListHelper.cs
+++ 15HtmlHelpersApp/15HtmlHelpersApp/App_Code/ListHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -12,14 +13,21 @@ namespace _15HtmlHelpersApp.App_Code
         public static HtmlString CreateList(this IHtmlHelper html,
             string[] items)
         {
-            string result = "<ul>";
+            TagBuilder ul = new TagBuilder("ul");
+            
             foreach (string item in items)
             {
-                result += $"<li>{item}</li>";
+                TagBuilder li = new TagBuilder("li");
+                // add text in li
+                li.InnerHtml.Append(item);
+                // add li in ul
+                ul.InnerHtml.AppendHtml(li);
             }
 
-            result += "</ul>";
-            return new HtmlString(result);
+            ul.Attributes.Add("class", "itemsList");
+            var writer = new System.IO.StringWriter();
+            ul.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
         }
     }
 }
--- 15HtmlHelpersApp/15HtmlHelpersApp/Controllers/HomeController.cs
+++ 15HtmlHelpersApp/15HtmlHelpersApp/Controllers/HomeController.cs
@@ -4,16 +4,40 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using _15HtmlHelpersApp.Models;
 
 namespace _15HtmlHelpersApp.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
+        {
+            List<Phone> phones = new List<Phone>
+            {
+                new Phone {Id=1, Name="iPhone 7 Pro", Price=680 },
+                new Phone {Id=2, Name="Galaxy 7 Edge", Price=640 },
+                new Phone {Id=3, Name="HTC 10", Price=500 },
+                new Phone {Id=4, Name="Honor 5X", Price=400 },
+            };
+
+            Phone phone = new Phone { Id = 1, Name = "Nexus 6P", Price = 49000 };
+            ViewBag.phone = phone;
+            ViewBag.Phones = new SelectList(phones, "Id", "Name");
+            return View(phone);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+
+        //[HttpPost]
+        //public IActionResult Create(Phone myPhone)
+        //{
+            
+        //}
 
         public IActionResult About()
         {
