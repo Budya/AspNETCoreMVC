5fc48f0e9a9927d29d9e81f21beb3333b0babf1c
Budya <budya2012@gmail.com>
2018-09-27 14:01:27 +0300

    CRUD app added

--- 18EFDataApp/18EFDataApp/Startup.cs
+++ 18EFDataApp/18EFDataApp/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using _18EFDataApp.Models;
 
 namespace _18EFDataApp
 {
@@ -23,6 +25,11 @@ namespace _18EFDataApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            // add context as service of app
+            services.AddDbContext<MobileContext>(options =>
+                options.UseSqlServer(connection));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
