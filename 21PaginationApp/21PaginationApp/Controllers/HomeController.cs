363b4bcdad354a1ac47990f2c348f1229e890959
Budya <budya2012@gmail.com>
2018-09-28 16:53:59 +0300

    PaginationTagHelper

---
+++ 21PaginationApp/21PaginationApp/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using _21PaginationApp.Models;
+
+namespace _21PaginationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UsersContext db;
+
+        public HomeController(UsersContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            int pageSize = 3;
+            IQueryable<User> source = db.Users.Include(x => x.Company);
+            var count = await source.CountAsync();
+            var items = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
+            IndexViewModel viewModel = new IndexViewModel
+            {
+                PageViewModel = pageViewModel, 
+                Users = items
+            };
+            return View(viewModel);
+        }
+        
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
