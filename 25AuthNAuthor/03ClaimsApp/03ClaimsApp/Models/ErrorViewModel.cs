a9e8f1c2b7c675ab19637bd4ac00cb8e20a75768
Budya <budya2012@gmail.com>
2018-10-17 16:55:37 +0300

    ClaimsApp add

---
+++ 25AuthNAuthor/03ClaimsApp/03ClaimsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _03ClaimsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
