631abc1b4bf3b36b20078310c9aefcd65fadaa5e
Budya <budya2012@gmail.com>
2018-10-17 15:17:20 +0300

    RolesApp add

---
+++ 25AuthNAuthor/02RolesApp/02RolesApp/Controllers/AccountController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using _02RolesApp.Models;
+
+namespace _02RolesApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private RolesAppContext _context;
+
+        public AccountController(RolesAppContext context)
+        {
+            _context = context;
+            DatabazeInitialize();
+        }
+
+        private void DatabazeInitialize()
+        {
+            if (!_context.Roles.Any())
+            {
+                string adminRoleName = "admin";
+                string userRoleName = "user";
+ 
+                string adminEmail = "admin@mail.ru";
+                string adminPassword = "123456";
+ 
+                // добавляем роли
+                Role adminRole = new Role { Name = adminRoleName };
+                Role userRole = new Role { Name = userRoleName };
+ 
+                _context.Roles.Add(userRole);
+                _context.Roles.Add(adminRole);
+ 
+                // добавляем администратора
+                _context.Users.Add(new User { Email = adminEmail, Password = adminPassword, Role = adminRole });
+ 
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    user = new User {Email = model.Email, Password = model.Password};
+                    Role userRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "user");
+                    if (userRole != null)
+                        user.Role = userRole;
+
+                    _context.Users.Add(user);
+                    await _context.SaveChangesAsync();
+
+                    await Authenticate(user);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Bad login or password");
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users
+                    .Include(u => u.Role)
+                    .FirstOrDefaultAsync(u => u.Email == model.Email &&
+                                              u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(user);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Bad login or password");
+            }
+
+            return View(model);
+        }
+
+        private async Task Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", 
+                ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+    }
+}
---
+++ 25AuthNAuthor/02RolesApp/02RolesApp/Models/RolesAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _02RolesApp.Models
+{
+    public class RolesAppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public RolesAppContext(DbContextOptions<RolesAppContext> options)
+            : base(options)
+        {
+        }
+    }
+}
