3b64bc42fb2e00c50c8d834a83b1e955c4c388a0
Budya <budya2012@gmail.com>
2018-10-24 08:52:53 +0300

    ReactApp add

---
+++ 27React/01ReactApp/01ReactApp/wwwroot/js/app.jsx
@@ -0,0 +1,10 @@
+﻿class Hello extends React.Component {
+    render() {
+        return <h1>HELLO, React.JS</h1>;
+    }
+}
+
+ReactDOM.render(
+    <Hello/>,
+    document.getElementById("content")
+);
